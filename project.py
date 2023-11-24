a6756761943acbd6d26ef41cf9cb58f9c2a03c09
Lajos Nagy <nagylali001@gmail.com>
2023-11-24 15:18:14 +0100

    Add required custom function boilerplate

--- project.py
+++ project.py
@@ -1,4 +1,17 @@
 def main():
+    print('Hello Final Project!')
+
+    custom_function_1()
+    custom_function_2()
+    custom_function_3()
+
+def custom_function_1():
+    ...
+
+def custom_function_2():
+    ...
+
+def custom_function_3():
     ...
 
 if __name__ == '__main__':
