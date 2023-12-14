9632b43677e3f7dc4bd01cc09405db6ccf614d45
Lajos Nagy <nagylali001@gmail.com>
2023-12-14 11:52:56 +0100

    Create window and add map widget

--- project.py
+++ project.py
@@ -1,9 +1,24 @@
+import tkinter
+import tkintermapview
+
+
 def main():
-    print('Hello Final Project!')
+
+    #create window
+    window = tkinter.Tk()
+    window.geometry(f'{1000}x{700}')
+    window.title('CS50 Final Project')
+
+    #create map widget
+    map_widget = tkintermapview.TkinterMapView(window, width=1000, height=700, corner_radius=0)
+    map_widget.grid(column=0, row=0)
+    
 
     custom_function_1()
     custom_function_2()
     custom_function_3()
+
+    window.mainloop()
 
 def custom_function_1():
     ...
